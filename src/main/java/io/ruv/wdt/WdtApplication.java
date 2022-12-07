7e1c8a12b299301faade05e46bd247dc70dd3fb8
ruv <brand20@yandex.ru>
2022-12-07 15:26:45 +0300

    barebone spring project

---
+++ src/main/java/io/ruv/wdt/WdtApplication.java
@@ -0,0 +1,13 @@
+package io.ruv.wdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ruv/wdt/WdtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ruv.wdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
